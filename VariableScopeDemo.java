d18f7305551644f01fb17c491da8c3f83174a34c
Kavya-natta <151709486+Kavya-natta@users.noreply.github.com>
2024-08-22 12:10:55 +0530

    Create VariableScopeDemo.java

---
+++ VariableScopeDemo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main{
+    
+    public static void main (String args[]){
+        for(int x=0;x<5;x++){
+            System.out.println("Hello world");
+        }
+        
+    }
+}
